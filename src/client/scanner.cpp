4a341dbff2e14621ef3bb001647785642cce199e
Lukasz Kidzinski <lukasz.kidzinski@gmail.com>
2015-01-04 13:17:40 +0100

    initial commit: scanner and server

---
+++ src/client/scanner.cpp
@@ -0,0 +1,98 @@
+/*This is the sample program to notify us for the file creation and file deletion takes place in “/tmp” directory*/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/inotify.h>
+#include <unistd.h>
+#include <dirent.h>
+
+#define EVENT_SIZE  ( sizeof (struct inotify_event) )
+#define EVENT_BUF_LEN     ( 1024 * ( EVENT_SIZE + 16 ) )
+
+int scan_dirs(char* path, int fd)
+{
+  int wd;
+  wd = inotify_add_watch( fd, path, IN_CREATE | IN_DELETE );
+  DIR *dir;
+  struct dirent *ent;
+  if ((dir = opendir (path)) != NULL) {
+    /* add all subdirectories */
+    while ((ent = readdir (dir)) != NULL) {
+      ent->d_name;
+      printf ("%s\n", ent->d_name);
+      if(DT_DIR == ent->d_type && strcmp(".",ent->d_name) && strcmp("..",ent->d_name))
+	scan_dirs(ent->d_name, fd);
+    }
+    closedir (dir);
+  } else {
+    /* could not open directory */
+    perror ("");
+    return EXIT_FAILURE;
+  }
+  return 0;
+}
+
+void handle_event ( struct inotify_event *event, int fd ) {
+  if ( event->mask & IN_CREATE ) {
+    if ( event->mask & IN_ISDIR ) {
+      printf( "New directory %s created.\n", event->name );
+      scan_dirs(event->name, fd);
+    }
+    else {
+      printf( "New file %s created.\n", event->name );
+    }
+  }
+  else if ( event->mask & IN_DELETE ) {
+    if ( event->mask & IN_ISDIR ) {
+      printf( "Directory %s deleted.\n", event->name );
+    }
+    else {
+      printf( "File %s deleted.\n", event->name );
+    }
+  }
+}
+
+int main( )
+{
+  int length, i = 0;
+  int fd;
+  int wd;
+  char buffer[EVENT_BUF_LEN];
+
+  /*creating the INOTIFY instance*/
+  fd = inotify_init();
+
+  /*checking for error*/
+  if ( fd < 0 ) {
+    perror( "inotify_init" );
+  }
+
+  scan_dirs((char*)".",fd);
+
+  while(length = read( fd, buffer, EVENT_BUF_LEN )){
+
+    /*checking for error*/
+    if ( length < 0 ) {
+      perror( "read" );
+    }  
+
+    /*actually read return the list of change events happens. Here, read the change event one by one and process it accordingly.*/
+    while ( i < length ) { 
+      struct inotify_event *event = ( struct inotify_event * ) &buffer[ i ];
+      if ( event->len ) {
+	handle_event(event);
+      }
+      i += EVENT_SIZE + event->len;
+    }
+    i = 0;
+  }
+
+  /*removing the “/tmp” directory from the watch list.*/
+   inotify_rm_watch( fd, wd );
+
+  /*closing the INOTIFY instance*/
+   close( fd );
+
+}
